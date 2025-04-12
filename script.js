cfe34d7a7fca23fab0b8fd944047efc9bd7df271
Moktan-git <moktanresham3@gmail.com>
2025-04-12 09:49:34 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let num1 = 2 //number
+// let number1 = num1 + 1
+// console.log('number1')
+
+// const happyMe = 'Resham Tamang'
+// console.log(happyMe)
+
+// let nameOfStudent = 'Resham Tamang' //String
+// let roolNo = 20
+// let student = true // boolean
+// console.log(nameOfStudent)
+// console.log(roolNo)
+// console.log(student)
+// const number2 = 3.5
+// console.log(Math.round(number2))
+
