87d1c7270d5ac6f09369c80de6d47deab531b3ec
Moktan-git <moktanresham3@gmail.com>
2025-04-12 11:18:21 +0545

    js Array and its method and objects

---
+++ day2.js
@@ -0,0 +1,22 @@
+// var name1 = 'Resham Tamang'
+// var name2 = 'Badri prasad'
+// var name3 = 'Michael jorden'
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
+
+var names = ['Resham Tamang','Badri prasad','Michael jorden']
+names[0] = 'Saugat Malla'
+console.log(names)
+// console.log(names[0]) //
+// console.log(names[1]) // Array 
+// console.log(names[2]) //
+
+// Objects
+const person1 = {
+  name: 'Resham Tamang',
+  age: 21,
+  addres: 'Sindhupalchok'
+}
+console.log(person1.name) // dot notation
+console.log(person1['name'])   // Bracket notation
